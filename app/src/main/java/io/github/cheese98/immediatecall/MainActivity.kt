dd46ab04ef45117b29efc886be02db74db802aa7
cheese98 <cheesej98@gmail.com>
2020-02-24 08:47:24 +0900

    create lockscreen package

--- app/src/main/java/io/github/cheese98/immediatecall/MainActivity.kt
+++ app/src/main/java/io/github/cheese98/immediatecall/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.github.cheese98.immediatecall.lockscreen.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,13 +13,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_main)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/io/github/cheese98/immediatecall/lockscreen/LockScreenActivity.kt
@@ -0,0 +1,11 @@
+package io.github.cheese98.immediatecall.lockscreen
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class LockScreenActivity : AppCompatActivity(){
+
+}
