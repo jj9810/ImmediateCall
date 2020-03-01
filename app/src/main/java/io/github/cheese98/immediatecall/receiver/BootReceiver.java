57f29b0b2aefced40b7daa9a52ab9a8f06455903
cheese98 <cheesej98@gmail.com>
2020-03-01 18:14:13 +0900

    receiver 패키지 추가

---
+++ app/src/main/java/io/github/cheese98/immediatecall/receiver/BootReceiver.java
@@ -0,0 +1,15 @@
+package io.github.cheese98.immediatecall.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
+        if(action.equals(Intent.ACTION_BOOT_COMPLETED)){
+            //활성화
+        }
+    }
+}
---
+++ app/src/main/java/io/github/cheese98/immediatecall/receiver/PacakgeReceiver.java
@@ -0,0 +1,15 @@
+package io.github.cheese98.immediatecall.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PacakgeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
+        if(action.equals(intent.ACTION_MY_PACKAGE_REPLACED)){
+            // 활성화
+        }
+    }
+}
