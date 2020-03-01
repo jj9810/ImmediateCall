e3e807032aa4703e3a620384bea4d5b905b1d3ea
cheese98 <cheesej98@gmail.com>
2020-03-01 18:30:56 +0900

    오타 수정 (Pacakge -> Package)

--- app/src/main/java/io/github/cheese98/immediatecall/receiver/PackageReceiver.java
+++ app/src/main/java/io/github/cheese98/immediatecall/receiver/PackageReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class PacakgeReceiver extends BroadcastReceiver {
+public class PackageReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent){
         String action = intent.getAction();
